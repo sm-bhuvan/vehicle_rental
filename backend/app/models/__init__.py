fe7f165270aa51efd8361fb7f966d35d517c81ac
sm-bhuvan <smbhuvantsi@gmail.com>
2025-09-06 22:16:02 +0530

    Revert "added backend folder"
    This reverts commit 810bc15a835e395122ac7f436ac0e6b9380cac5c.

--- backend/app/models/__init__.py
+++
@@ -1,6 +0,0 @@
-from .user import User
-from .vehicle import Vehicle
-from .booking import Booking
-from .payment import Payment
-
-__all__ = ['User', 'Vehicle', 'Booking', 'Payment']
--- backend/app/routes/__init__.py
+++
@@ -1,7 +0,0 @@
-from .auth import auth_bp
-from .users import users_bp
-from .vehicles import vehicles_bp
-from .bookings import bookings_bp
-from .payments import payments_bp
-
-__all__ = ['auth_bp', 'users_bp', 'vehicles_bp', 'bookings_bp', 'payments_bp']
